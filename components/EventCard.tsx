fd306411b6c8a7d3511c9a5346071ff6ae046f25
coderyeasin <yeasin3233@gmail.com>
2025-11-30 15:23:52 +0600

    card-done

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+import {MdOutlineLocationOn} from "react-icons/md";
+import {FaRegCalendarAlt, FaRegClock} from "react-icons/fa";
 
 interface Props {
     title: string;
@@ -11,12 +13,25 @@ interface Props {
 
 }
 
-const EventCard = ({title,image}: Props) => {
+const EventCard = ({title,image, slug, location, date, time}: Props) => {
     return (
-        <Link href={`/events`} id="event-card" className="event-card">
+        <Link href={`/events/${slug}`} id="event-card" className="event-card">
             <Image src={image} alt={title} width={410} height={300} className="poster" />
-
+            <div className="card-content">
+                <MdOutlineLocationOn width={14} height={14} />
+                <p>{location}</p>
+            </div>
         <p className="truncate title">{title}</p>
+            <div className="flex flex-row gap-2 justify-between">
+                <div className="card-content">
+                    <FaRegCalendarAlt width={14} height={14} />
+                    <p>{date}</p>
+                </div>
+                <div className="card-content">
+                    <FaRegClock width={14} height={14} />
+                    <p>{time}</p>
+                </div>
+            </div>
         </Link>
     )
 }
