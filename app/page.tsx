6bab55f08e326f4af2803489578de7fb8b192276
coderyeasin <yeasin3233@gmail.com>
2025-12-05 23:31:11 +0600

    polish

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,31 @@
 import ExploreBtn from "@/components/exploreBtn";
 import EventCard from "@/components/EventCard";
-import {events} from "@/lib/constants";
-
-// const events = [
-//     {
-//         image:'/images/event-1.png',
-//         title:'Event 1',
-//         slug:'event-1',
-//         location: 'Location 1',
-//         date: 'Date 1',
-//         time: 'Time 1',
-//
-//     },
-//     {image:'/images/event-1.png',title:'Event 2'},
-//     {image:'/images/event-1.png',title:'Event 3'},
-//     {image:'/images/event-1.png',title:'Event 4'},
-//     {image:'/images/event-2.png',title:'Event 5'},
-// ]
+import { events } from "@/lib/constants";
 
 const Page = () => {
-    return (
-        <section>
-           <div>
-               <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
-               <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in One Place</p>
+  return (
+    <section>
+      <div>
+        <h1 className="text-center">
+          The Hub for Every Dev <br /> Event You Can't Miss
+        </h1>
+        <p className="text-center mt-5">
+          Hackathons, Meetups and Conferences, All in One Place
+        </p>
+      </div>
+      <ExploreBtn />
 
-           </div>
-            <ExploreBtn />
-
-            <div className="mt-20 space-y-7 events-section">
-                <h2>Featured Events</h2>
-                <ul className="events-cards">
-                    {events.map((event) => (
-                        <li key={event.title}>
-                            <EventCard {...event} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
-    )
-}
-export default Page
+      <div className="mt-20 space-y-7 events-section">
+        <h2>Featured Events</h2>
+        <ul className="events-cards">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+export default Page;
