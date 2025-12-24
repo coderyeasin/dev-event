4da7d55c9f96f7464778dbc565064e8a5fe6255e
coderyeasin <yeasin3233@gmail.com>
2025-12-24 22:32:58 +0600

    polish

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
     <section>
       <div>
         <h1 className="text-center">
-          The Hub for Every Dev <br /> Event You Can't Miss
+          The Hub for Every Dev <br /> Event You Can not Miss
         </h1>
         <p className="text-center mt-5">
           Hackathons, Meetups and Conferences, All in One Place
