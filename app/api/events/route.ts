711c3fc6d8abbd7452203a085ae266ca24dbc20c
coderyeasin <yeasin3233@gmail.com>
2025-12-25 16:05:00 +0600

    api-routes-for-events

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,7 +1,19 @@
+import connectToDatabase from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
+    await connectToDatabase();
+    const formData = await req.formData();
+    let event;
+    try {
+      event = Object.fromEntries(formData.entries());
+    } catch (e) {
+      return NextResponse.json({
+        message: "Invalid Form Data Format",
+        status: 400,
+      });
+    }
   } catch (e) {
     console.log(e);
     return NextResponse.json({
