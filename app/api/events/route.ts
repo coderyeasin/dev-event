8cb04998ce241fd65e1edde857ffb1c0d1801197
coderyeasin <yeasin3233@gmail.com>
2025-12-26 21:00:49 +0600

    image-up-done-using-cloudinary

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,7 @@
 import { Event } from "@/database/event.model";
 import connectToDatabase from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
 
 export async function POST(req: NextRequest) {
   try {
@@ -15,7 +16,39 @@ export async function POST(req: NextRequest) {
         status: 400,
       });
     }
+
+    // image as file
+    const file = formData.get("image") as File | null;
+    if (!file) {
+      return NextResponse.json({
+        message: "Image file is required",
+        status: 400,
+      });
+    }
+    // convert file to buffer
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          {
+            resource_type: "image",
+            folder: "dev-event",
+          },
+          (error, result) => {
+            if (error) return reject(error);
+
+            resolve(result);
+          }
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
+
     const createdEvent = await Event.create(event);
+
     return NextResponse.json({
       message: "Event Created Successfully",
       status: 201,
