faeb12e127b68b767b785a1d48fdbda1a92f3874
coderyeasin <yeasin3233@gmail.com>
2026-01-31 22:56:11 +0600

    polish

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,16 +1,12 @@
 import Login from "@/components/Login/Login";
+import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
-import { Suspense } from "react";
 
 export default async function LoginPage() {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   if (session) {
     redirect("/");
   }
-  return (
-    <Suspense fallback={<p>loading...</p>}>
-      <Login />
-    </Suspense>
-  );
+  return <Login />;
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,16 +1,12 @@
 import RegisterPage from "@/components/Register/Register";
+import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
-import { Suspense } from "react";
 
 export default async function Register() {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   if (session) {
     redirect("/");
   }
-  return (
-    <Suspense fallback={<p>loading...</p>}>
-      <RegisterPage />
-    </Suspense>
-  );
+  return <RegisterPage />;
 }
