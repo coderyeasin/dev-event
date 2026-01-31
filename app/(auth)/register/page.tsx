b657ea612c1d67842cf57128be0f28fcbc0b72e8
coderyeasin <yeasin3233@gmail.com>
2026-01-31 23:04:24 +0600

    polish

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -2,11 +2,16 @@ import RegisterPage from "@/components/Register/Register";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
 export default async function Register() {
   const session = await getServerSession(authOptions);
   if (session) {
     redirect("/");
   }
-  return <RegisterPage />;
+  return (
+    <Suspense fallback={<p>loading...</p>}>
+      <RegisterPage />
+    </Suspense>
+  );
 }
