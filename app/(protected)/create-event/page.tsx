8992db2d5a71d2517f25589a8d8a1808449628ee
coderyeasin <yeasin3233@gmail.com>
2026-02-01 23:37:35 +0600

    furnish

--- app/(protected)/create-event/page.tsx
+++ app/(protected)/create-event/page.tsx
@@ -1,26 +1,14 @@
-"use client";
 import CreateEvent from "@/components/CreateEvent/CreateEvent";
-import { useSession } from "next-auth/react";
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 
-export default function Page() {
-  const { data: session, status } = useSession();
-
-  if (status === "loading") {
-    return (
-      <main className="py-12 flex justify-center items-center min-h-[40vh]">
-        <span className="text-lg text-teal-700 animate-pulse">Loading...</span>
-      </main>
-    );
-  }
-
+const page = () => {
   return (
     <main className="py-12">
-      {session?.user ? (
-        <Suspense fallback={<p>loading...</p>}>
-          <CreateEvent />
-        </Suspense>
-      ) : null}
+      <Suspense fallback={<p>loading...</p>}>
+        <CreateEvent />
+      </Suspense>
     </main>
   );
-}
+};
+
+export default page;
