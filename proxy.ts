a0f41a2cd576515f2f3bce9efc886eb1a0543e57
coderyeasin <yeasin3233@gmail.com>
2026-02-03 16:13:36 +0600

    polish

--- proxy.ts
+++ proxy.ts
@@ -3,25 +3,22 @@ import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function proxy(req: NextRequest) {
-  // getToken automatically handles the secure vs non-secure cookie names
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
   });
 
   const isAuthenticated = !!token;
+  const pathname = req.nextUrl.pathname;
 
-  // If user is NOT authenticated and trying to access a protected page
-  if (req.nextUrl.pathname.startsWith("/create-event") && !isAuthenticated) {
+  if (pathname.startsWith("/create-event") && !isAuthenticated) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  // If user IS authenticated and tries to visit login, redirect them to home
-  if (req.nextUrl.pathname.startsWith("/login") && isAuthenticated) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-  // If user IS authenticated and tries to visit register, redirect them to home
-  if (req.nextUrl.pathname.startsWith("/register") && isAuthenticated) {
+  if (
+    (pathname.startsWith("/login") || pathname.startsWith("/register")) &&
+    isAuthenticated
+  ) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
@@ -29,5 +26,5 @@ export async function proxy(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/create-event", "/login", "/register"],
+  matcher: ["/create-event/:path*", "/login", "/register"],
 };
