6714ab53dccd69e7e3911400c99e74b4ff60d578
coderyeasin <yeasin3233@gmail.com>
2026-02-28 05:12:45 +0600

    polish

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,12 @@
-import Login from "@/components/Login/Login";
+import dynamic from "next/dynamic";
+import React, { Suspense } from "react";
+
+const Login = dynamic(() => import("@/components/Login/Login"), { ssr: true });
 
 export default function LoginPage() {
-  return <Login />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Login />
+    </Suspense>
+  );
 }
